84681e7b33b290d7d69f99e1d4faeafdf94d97c7
ваше имяwlad999 <wladmail25@gmeil.com>
2019-10-04 14:30:37 +0300

    Updates

---
+++ precache-manifest.d3d63dccc645071d984b4be47bf4c39d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2143b6ae912b20abf1aeab9d536c66a8",
+    "url": "/Sanna/index.html"
+  },
+  {
+    "revision": "d94fcf108942e0fdf6cd",
+    "url": "/Sanna/static/css/main.55b80942.chunk.css"
+  },
+  {
+    "revision": "3ad8125b846d7a606ca0",
+    "url": "/Sanna/static/js/2.3645feea.chunk.js"
+  },
+  {
+    "revision": "d94fcf108942e0fdf6cd",
+    "url": "/Sanna/static/js/main.e243de0b.chunk.js"
+  },
+  {
+    "revision": "cf0b7b9914f1ddb994ea",
+    "url": "/Sanna/static/js/runtime~main.a4f025a7.js"
+  },
+  {
+    "revision": "a6f66922c08709037ca326697513dbb6",
+    "url": "/Sanna/static/media/slider0.a6f66922.jpg"
+  },
+  {
+    "revision": "67b5507da87e41f4974fda2110a88194",
+    "url": "/Sanna/static/media/slider2.67b5507d.jpg"
+  }
+]);
